3c736f5912054878dcb64a75cbefd31459ceb517
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-04-15 01:08:00 -0400

    Declaracion de Variables Enteras, con valor maximo soportado

--- Holamundo.java
+++ Holamundo.java
@@ -1,10 +1,19 @@
 public class Holamundo {
    public static void main (String[] args){
        System.out.println("Hola Mundo"); // con salto de linea (comentario "//")
-       System.out.print("linea sin salto de linea"); // sin salto de linea
+       System.out.println("linea sin salto de linea"); // sin salto de linea
    /* esto es un ejemplo 
       de  comentario de 
       varias lineas
     */
+    byte  b = 127; //declaracion de variable byte -128 a 127
+    short s = 32767; //declaracion de variable short -32768 a 32767  
+    int   i = 2147483647; //declaracion de variable short -2147483648 a 2147483647  
+    long  l = 1999999999; //declaracion de variable short -9223372854775808 a 9223372854775807
+    System.out.println("Valor de Variable Entera tipo byte "+ b);    
+    System.out.println("Valor de Variable Entera tipo Short "+ s);
+    System.out.println("Valor de Variable Entera tipo int "+ i);
+    System.out.println("Valor de Variable Entera tipo int "+ l);
+
     }
 }
