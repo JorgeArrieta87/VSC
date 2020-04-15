fa7d29f201428ce10bd08f3e868ac4cab3d45a15
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-04-15 19:59:08 -0400

    Se añade Variables con decimal tipo float, double, asi como tambien referencias visuales

--- Holamundo.java
+++ Holamundo.java
@@ -6,14 +6,17 @@ public class Holamundo {
       de  comentario de 
       varias lineas
     */
-    byte  b = 127; //declaracion de variable byte -128 a 127
-    short s = 32767; //declaracion de variable short -32768 a 32767  
-    int   i = 2147483647; //declaracion de variable short -2147483648 a 2147483647  
-    long  l = 1999999999; //declaracion de variable short -9223372854775808 a 9223372854775807
+    byte   b = 127;        //declaracion de variable byte -128 a 127
+    short  s = 32767;      //declaracion de variable short -32768 a 32767  
+    int    i = 2147483647; //declaracion de variable int -2147483648 a 2147483647  
+    long   l = 1999999999; //declaracion de variable long -9223372854775808 a 9223372854775807
+    float  f = 1.4f;       //declaracion de variable float 1.4e-045 a 3.4e+038
+    double d = 4.9d;       ////declaracion de variable double 4.9e-324 a 1.8e+308 
     System.out.println("Valor de Variable Entera tipo byte "+ b);    
     System.out.println("Valor de Variable Entera tipo Short "+ s);
     System.out.println("Valor de Variable Entera tipo int "+ i);
-    System.out.println("Valor de Variable Entera tipo int "+ l);
-
+    System.out.println("Valor de Variable Entera tipo long "+ l);
+    System.out.println("Valor de Variable Entera tipo float "+ f);
+    System.out.println("Valor de Variable Entera tipo double "+ d);
     }
 }
