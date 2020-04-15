e82fe210e3f4f8f6654305771ab5c399adad8cb1
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-04-15 00:26:27 -0400

    Se agrega comentario de varias lineas

--- Holamundo.java
+++ Holamundo.java
@@ -1,6 +1,10 @@
 public class Holamundo {
    public static void main (String[] args){
-       System.out.println("Hola Mundo");
-       System.out.print("linea sin salto de linea");
-   }
+       System.out.println("Hola Mundo"); // con salto de linea (comentario "//")
+       System.out.print("linea sin salto de linea"); // sin salto de linea
+   /* esto es un ejemplo 
+      de  comentario de 
+      varias lineas
+    */
+    }
 }
