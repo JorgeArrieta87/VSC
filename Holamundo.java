5f5252e25dbe901626ea5529cdd70157bca32daa
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-04-15 20:23:03 -0400

    Se añade Variable Tipo CHar, guarda solo un caracter

--- Holamundo.java
+++ Holamundo.java
@@ -11,12 +11,16 @@ public class Holamundo {
     int    i = 2147483647; //declaracion de variable int -2147483648 a 2147483647  
     long   l = 1999999999; //declaracion de variable long -9223372854775808 a 9223372854775807
     float  f = 1.4f;       //declaracion de variable float 1.4e-045 a 3.4e+038
-    double d = 4.9d;       ////declaracion de variable double 4.9e-324 a 1.8e+308 
-    System.out.println("Valor de Variable Entera tipo byte "+ b);    
+    double d = 4.9d;       //declaracion de variable double 4.9e-324 a 1.8e+308 
+    char   u; u = 'u';     //declaracion de variable tipo Char, almacenda solo un caracter, debe declararse, e inicializarce 
+    
+    System.out.println("Valor de Variable Entera tipo" + b);    
     System.out.println("Valor de Variable Entera tipo Short "+ s);
     System.out.println("Valor de Variable Entera tipo int "+ i);
     System.out.println("Valor de Variable Entera tipo long "+ l);
     System.out.println("Valor de Variable Entera tipo float "+ f);
     System.out.println("Valor de Variable Entera tipo double "+ d);
+    System.out.println("Valor de Variable Entera tipo char "+ u);
+
     }
 }
