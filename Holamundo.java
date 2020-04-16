8025667cb4ff7d9d244f2ddb6602699cbfdf3330
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-04-15 20:27:08 -0400

    Se añade variable de tipo Boolena

--- Holamundo.java
+++ Holamundo.java
@@ -13,14 +13,15 @@ public class Holamundo {
     float  f = 1.4f;       //declaracion de variable float 1.4e-045 a 3.4e+038
     double d = 4.9d;       //declaracion de variable double 4.9e-324 a 1.8e+308 
     char   u; u = 'u';     //declaracion de variable tipo Char, almacenda solo un caracter, debe declararse, e inicializarce 
+    boolean boo = true;     //declaracion de variable Booleana
     
     System.out.println("Valor de Variable Entera tipo" + b);    
     System.out.println("Valor de Variable Entera tipo Short "+ s);
     System.out.println("Valor de Variable Entera tipo int "+ i);
     System.out.println("Valor de Variable Entera tipo long "+ l);
-    System.out.println("Valor de Variable Entera tipo float "+ f);
-    System.out.println("Valor de Variable Entera tipo double "+ d);
-    System.out.println("Valor de Variable Entera tipo char "+ u);
-
+    System.out.println("Valor de Variable Decimal tipo float "+ f);
+    System.out.println("Valor de Variable Decimal tipo double "+ d);
+    System.out.println("Valor de Variable tipo char "+ u);
+    System.out.println("Valor de Variable tipo Booleana "+ boo);
     }
 }
