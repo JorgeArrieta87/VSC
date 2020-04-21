fdb0b1ab9c0e57226739f79890a567a1703a5c3a
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-04-20 23:39:30 -0400

    Practica 4 completa

--- ejercicio4.java
+++ ejercicio4.java
@@ -4,7 +4,8 @@ public class ejercicio4 {
     public static void main(String[]args){
     Scanner entradaf = new Scanner(System.in);
     float avf,acf,sumaf; 
-    
+    final int sueldo = 1000;
+    final int comisionv = 150;
     System.out.println("Ingrese el monto de la venta de los Autos");
     avf = entradaf.nextFloat();
     System.out.println("Ingrese la cantidad de los Autos");
@@ -14,9 +15,9 @@ public class ejercicio4 {
     avf = (avf*5)/100;
     System.out.println("El monto de la comision por volumen de venta  "+ avf);
    
-    acf = acf*150;
+    acf = acf*comisionv;
     System.out.println("El monto de  comision por venta  "+ acf);
-    sumaf = (avf+acf+1000);
+    sumaf = (avf+acf+sueldo);
     System.out.println("El sueldo del vendedor es   "+ sumaf);
 
     }    
