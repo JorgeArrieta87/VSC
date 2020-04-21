e57919cff0910245038f377ca138c337e1be8264
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-04-20 21:53:09 -0400

    Practica 2

--- ejercicio2.java
+++ ejercicio2.java
@@ -3,14 +3,22 @@ public class ejercicio2 {
 
     public static void main(String[]args){
     Scanner entradaf = new Scanner(System.in);
-    float n1f,n2f,n3f,sumaf; 
+    float vhf,d1f,d2f,d3f,d4f, d5f,d6f,d7f,sumaf; 
     
-    System.out.println("Digite Tres Notas");
-    n1f = entradaf.nextFloat();
-    n2f = entradaf.nextFloat();
-    n3f = entradaf.nextFloat();
-    sumaf = n1f + n2f + n3f;
-    System.out.println("La nota es  "+ sumaf);
+    System.out.println("Digite cantidad de horas trabajadas por dia, del dia 1 al 7");
+    d1f = entradaf.nextFloat();
+    d2f = entradaf.nextFloat();
+    d3f = entradaf.nextFloat();
+    d4f = entradaf.nextFloat();
+    d5f = entradaf.nextFloat();
+    d6f = entradaf.nextFloat();
+    d7f = entradaf.nextFloat();
+    
+    System.out.println("Indique el valor de la hora, para el empleado");
+    vhf = entradaf.nextFloat();
+    
+    sumaf = (d1f + d2f + d3f + d4f + d5f + d6f + d7f)*vhf;
+    System.out.println("La cantidad a pagar es  "+ sumaf);
 
     }    
 
