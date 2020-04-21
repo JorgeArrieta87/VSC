fde98d70a1e832bcf57ab8254166664a972dac39
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-04-20 23:58:49 -0400

    Ahora si esta correcto

--- ejercicio4.java
+++ ejercicio4.java
@@ -4,16 +4,15 @@ public class ejercicio4 {
     public static void main(String[]args){
     Scanner entradaf = new Scanner(System.in);
     float avf,acf,sumaf; 
-    final int porcenventa = 5;
+    final int porcenventaf = 5;
     final int sueldo = 1000;
     final int comisionv = 150;
-    System.out.println("Ingrese el monto de la venta de los Autos");
+    System.out.println("Ingrese el valor del Auto");
     avf = entradaf.nextFloat();
     System.out.println("Ingrese la cantidad de los Autos");
     acf = entradaf.nextFloat();
     
-    
-    avf = (avf*porcenventa)/100;
+    avf = ((avf*acf)*porcenventaf)/100;
     System.out.println("El monto de la comision por volumen de venta  "+ avf);
    
     acf = acf*comisionv;
