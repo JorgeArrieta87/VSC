38e1c26d9100f16e5a25c5dd1caecc09430119f9
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-04-29 01:15:06 -0400

    ap3sec3sentenciaswitch

--- cap3sec3sentenciaswitch.java
+++ cap3sec3sentenciaswitch.java
@@ -1,40 +1,34 @@
 import javax.swing.JOptionPane;
-public class cap3sec2sentifelse {
+import javax.swing.ImageIcon;   //libreria que permite importar un icono
+public class cap3sec3sentenciaswitch {
 
     public static void main(String[]args){
     int valor1, valor2;
+    ImageIcon icon = new ImageIcon("C:/Users/exjaros/Desktop/VSC/icon.png");
     //A continuacion se captura los datos desde una ventana emergente
-    valor1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese Valor", "Entrada N-1", JOptionPane.ERROR_MESSAGE));
-    valor2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese Valor", "Entrada N-2", JOptionPane.ERROR_MESSAGE));
-     
-    //Operacion  !=  Diferencia
-    if (valor1 != valor2){
-        System.out.println("(!=) El "+ valor1+" es diferente de "+ valor2);
-    }
-    //Operacion  || Operador or (o)
-    if (valor1 < valor2 || valor1 > valor2){
-        System.out.println("(||) El "+ valor1+" es  diferente que "+ valor2);
-    }
-    //Operacion < Menor que
-    if (valor1 < valor2){
-        System.out.println("El "+ valor1+"  es menor que "+ valor2); 
-    }
-    //Operacion > Mayor que
-    if (valor1 > valor2){
-        System.out.println("El "+ valor1+"  es mayor que "+ valor2);
-    }
+    valor1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese Valor menor que 5 ", "Entrada N-1", JOptionPane.ERROR_MESSAGE));
+    valor2 = 5;
+
     //Operacion && Operador and (y)
-    if (valor1 <= valor2 && valor1 >= valor2){
-        System.out.println("(&&) El "+ valor1+"  es  igual que "+ valor2); 
-    }
-    //Operacion == Igualdad
-    if (valor1 == valor2){
-        System.out.println("(==) El "+ valor1+" es  igual que "+ valor2);
-    }
-    //si no (ejecuta instruccion)
+    if (valor1 <= valor2){
+        switch (valor1) {
+            case 1 : JOptionPane.showMessageDialog(null,     "El valor Ingresado es " + valor1,   "Muestra de Valor Caso "+ valor1,   JOptionPane.PLAIN_MESSAGE,  icon);
+            break;
+            case 2 : JOptionPane.showMessageDialog(null,     "El valor Ingresado es " + valor1,   "Muestra de Valor Caso "+ valor1,   JOptionPane.PLAIN_MESSAGE,  icon);
+            break;
+            case 3 : JOptionPane.showMessageDialog(null,     "El valor Ingresado es " + valor1,   "Muestra de Valor Caso "+ valor1,   JOptionPane.PLAIN_MESSAGE,  icon);
+            break;
+            case 4 : JOptionPane.showMessageDialog(null,     "El valor Ingresado es " + valor1,   "Muestra de Valor Caso "+ valor1,   JOptionPane.PLAIN_MESSAGE,  icon);
+            break;
+            case 5 : JOptionPane.showMessageDialog(null,     "El valor Ingresado es " + valor1,   "Muestra de Valor Caso "+ valor1,   JOptionPane.PLAIN_MESSAGE,  icon);
+            break;
+            default: JOptionPane.showMessageDialog(null,     "El valor Ingresado es " + valor1,   "Muestra de Valor Caso Errado",   JOptionPane.PLAIN_MESSAGE,  icon);
+            break;
+        }
+        }
     else
     {
-        System.out.println("(else) El "+ valor1+" es diferente de "+ valor2); 
+        System.out.println("(else) El "+ valor1+" no es una opcion valida"); 
     }    
   }
 }
