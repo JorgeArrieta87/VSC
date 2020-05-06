483af68395d76389b43679c6bb375a86ffc74442
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-05-06 00:43:59 -0400

    Sec3cap10Ejer6Determinar

---
+++ Sec3cap10Ejer6Determinar.java
@@ -0,0 +1,38 @@
+import javax.swing.JOptionPane;
+
+public class Sec3cap10Ejer6Determinar {
+
+    public static void main(String []args) {
+    int numero1, numero2;
+
+
+    numero1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese un numero1", "Validacion de Numero1 Par/inpar", JOptionPane.ERROR_MESSAGE));    
+    numero2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese un numero2", "Validacion de Numero2 Par/inpar", JOptionPane.ERROR_MESSAGE));    
+    
+    
+    if (numero1  == 0 ){ 
+        
+        JOptionPane.showMessageDialog(null, "El numero "+ numero1 + "no es posible categorizarlo ", "Validacion de Numero Invalido", JOptionPane.ERROR_MESSAGE);
+    } 
+    else if (numero1%2 == 0 ){ 
+        
+        JOptionPane.showMessageDialog(null, "El numero "+ numero1 + " es par", "Validacion de Numero Par", JOptionPane.ERROR_MESSAGE);
+    } 
+    else {
+        JOptionPane.showMessageDialog(null, "El numero "+ numero1 + " es inpar", "Validacion de Numero InPar", JOptionPane.ERROR_MESSAGE);
+        }
+    
+        if (numero2  == 0 ){ 
+        
+            JOptionPane.showMessageDialog(null, "El numero "+ numero2 + "no es posible categorizarlo ", "Validacion de Numero Invalido", JOptionPane.ERROR_MESSAGE);
+        } 
+        else if (numero2%2 == 0 ){ 
+            
+            JOptionPane.showMessageDialog(null, "El numero "+ numero2 + " es par", "Validacion de Numero Par", JOptionPane.ERROR_MESSAGE);
+        } 
+        else {
+            JOptionPane.showMessageDialog(null, "El numero "+ numero2 + " es inpar", "Validacion de Numero InPar", JOptionPane.ERROR_MESSAGE);
+            }
+    
+    }
+}
