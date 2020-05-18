6d88b2b4cf5aaf44250b57f21c37a37ef392b977
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-05-17 21:07:59 -0400

    Sec4cap3Ciclodowhile

---
+++ Sec4cap3Ciclodowhile.java
@@ -0,0 +1,14 @@
+
+public class Sec4cap3Ciclodowhile {
+    public static void main (String []args){
+        int i = 0;
+        do {
+            ++i;
+        }
+        while (i < 9); //while "mientras"
+        {
+            
+            System.out.println(i);
+        }
+    }
+}
