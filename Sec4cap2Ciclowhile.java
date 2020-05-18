e052442fdb868d243b877919a7640ab27d3978d1
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-05-17 21:02:13 -0400

    Sec4cap2Ciclowhile

--- Sec4cap2Ciclowhile.java
+++ Sec4cap2Ciclowhile.java
@@ -3,7 +3,6 @@ public class Sec4cap2Ciclowhile {
     public static void main (String []args){
         int i = 0;
         while (i < 9){
-            System.out.println(i);
             ++i;
             System.out.println(i);
         }
