0410b1d1da9bd12e7775e62a3998044c890a8465
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-05-17 21:30:58 -0400

    Sec4cap3Ciclodowhile

--- Sec4cap3Ciclodowhile.java
+++ Sec4cap3Ciclodowhile.java
@@ -1,8 +1,10 @@
+//do While, ejecuta el ciclo por lo menos una vez, y posterior evalua la condicion
+import java.util.Scanner; 
 
 public class Sec4cap3Ciclodowhile {
     public static void main (String []args){
         int i = 0;
-        do {
+        do {            //do "hacer" 
             ++i;
         }
         while (i < 9); //while "mientras"
@@ -10,5 +12,17 @@ public class Sec4cap3Ciclodowhile {
             
             System.out.println(i);
         }
+       
+       // segundo ejercisio dentro de la misma clase
+        int contador;
+        i =0;
+        Scanner  entrada = new Scanner(System.in);
+        System.out.print("Digite el Tope del Conteo");
+        contador = entrada.nextInt();
+
+        do {
+            System.out.println(i);
+            i+=3; // permite saltos de 3 en 3
+        } while (i < contador);
     }
 }
