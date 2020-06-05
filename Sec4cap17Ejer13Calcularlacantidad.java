f089b8b7947313eec7a356bdad4a04a348cf7dc6
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-06-05 00:08:50 -0400

    Sec4cap17Ejer13Calcularlacantidad

--- Sec4cap17Ejer13Calcularlacantidad.java
+++ Sec4cap17Ejer13Calcularlacantidad.java
@@ -2,27 +2,33 @@ import javax.swing.JOptionPane;
 public class  Sec4cap17Ejer13Calcularlacantidad {
     public static void main (String[]args){
     int numero, positivo, negativo, suma, resta, cero;  
-    numero= 0; positivo = 0; negativo = 0; suma = 0; resta = 0; cero = 0; 
-    for(int i =0; i == 10; i++ ) { 
+    positivo = 0; negativo = 0; suma = 0; resta = 0; cero = 0; 
+    for(int i =0; i <= 9; i++ ) { 
         numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese un numero", "Media Positiva Negativa ", JOptionPane.ERROR_MESSAGE));
             if (numero > 0)
             {
             positivo += numero;
-            ++suma;
+            suma++;
             }    
             else if (numero < 0)
             {
                 negativo += numero;
-                ++resta;  
+            ++resta;  
             }
             else
             {
                 ++cero;
             }
+            
         }
-        positivo /= suma;
+        
+       if (suma !=0){
+           positivo /= suma;
+       }
+      if  (resta !=0){
         negativo /= resta;
-        JOptionPane.showMessageDialog(null, "La media de los Numeros Positivos es "+ positivo + "\nLa media de los Numeros Positivos es "+ negativo +"\n   La Cantidad de Ceros es "+ cero,  "Media Positiva Negativa ", JOptionPane.ERROR_MESSAGE);           
+       }
+       JOptionPane.showMessageDialog(null, "La media de los Numeros Positivos es "+ positivo + "\nLa media de los Numeros Negativos es "+ negativo +"\n   La Cantidad de Ceros es "+ cero,  "Media Positiva Negativa ", JOptionPane.ERROR_MESSAGE);           
     }
 
 }
