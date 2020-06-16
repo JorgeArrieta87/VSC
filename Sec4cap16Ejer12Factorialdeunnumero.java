9e07035f11f3413294c12d19c3e0fe40251ece51
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-06-15 20:48:24 -0400

    Sec4cap18Ejer14Pedirsueldosmostrar

--- Sec4cap16Ejer12Factorialdeunnumero.java
+++ Sec4cap16Ejer12Factorialdeunnumero.java
@@ -4,9 +4,13 @@ public class  Sec4cap16Ejer12Factorialdeunnumero {
     int numero, veces = 1;
        long suma = 1; 
        numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese un numero", "Validacion de Sumatoria", JOptionPane.ERROR_MESSAGE));
-      for (int i =1; i<numero ; i++){
+     if (numero > 0){
+       for (int i =1; i<numero ; i++){
             suma *= veces++;
-            System.out.println(suma+"    "+veces);    
-      }
+            System.out.println(suma+"    "+veces);
+          }
+         
+        }
+    else  System.out.println("Numero Invalido");
     }
 }
---
+++ Sec4cap18Ejer14Pedirsueldosmostrar.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane;
+public class  Sec4cap18Ejer14Pedirsueldosmostrar {
+    public static void main (String[]args){
+    int numero, positivo, negativo, suma, resta, cero;  
+    positivo = 0; negativo = 0; suma = 0; resta = 0; cero = 0; 
+    for(int i =0; i <= 9; i++ ) { 
+        numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese un numero", "Media Positiva Negativa ", JOptionPane.ERROR_MESSAGE));
+            if (numero > 0)
+            {
+            positivo += numero;
+            }    
+            else if (numero > 1000)
+            {
+                suma++;  
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "Error Numero Invalido\n La media de los Sueldos deben ser Positivos\n",  "Media Sueldos", JOptionPane.ERROR_MESSAGE);;
+            i--;
+            }
+            
+        } 
+       
+       JOptionPane.showMessageDialog(null, "La suma de los Sueldos es "+ positivo + "\n Los que superan los 1000 son \n"+ suma,  "Media Sueldos", JOptionPane.ERROR_MESSAGE);           
+    }
+
+}
