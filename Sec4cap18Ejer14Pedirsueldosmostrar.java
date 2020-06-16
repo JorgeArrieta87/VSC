6cbb8fa9e40e003cc7bebd9285a97921e7bb29e1
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-06-15 22:05:17 -0400

    Sec4cap18Ejer14Pedirsueldosmostrar

--- Sec4cap18Ejer14Pedirsueldosmostrar.java
+++ Sec4cap18Ejer14Pedirsueldosmostrar.java
@@ -1,27 +1,52 @@
 import javax.swing.JOptionPane;
 public class  Sec4cap18Ejer14Pedirsueldosmostrar {
     public static void main (String[]args){
-    int numero, positivo,  suma;  
-    positivo = 0;  suma = 0;  
-    for(int i =0; i <= 9; i++ ) { 
-        numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese un numero", "Media Positiva Negativa ", JOptionPane.ERROR_MESSAGE));
-            if (numero > 0)
+    int edades, proedades,  suma, sumaalt; 
+    float alturas, proalturas;
+    proedades = 0;  suma = 0; alturas = 0; proalturas = 0;  sumaalt = 0;
+    for(int i =0; i <=4; i++ ) { 
+        edades = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese Edad de Alumno "+(i+1), "Recoleccion de Edades ", JOptionPane.ERROR_MESSAGE));
+            if (edades > 0)
             {
-            positivo += numero;
-            if (numero > 1000)
+            proedades += edades;
+            if (edades > 18)
                 {
                 suma++;  
                 }
             }
             else
             {
-                JOptionPane.showMessageDialog(null, "Error Numero Invalido\n La media de los Sueldos deben ser Positivos\n",  "Media Sueldos", JOptionPane.ERROR_MESSAGE);;
+                JOptionPane.showMessageDialog(null, "Edad Invalida\n Ingrese un valor Mayor a 0\n",  "Recoleccion de Edades ", JOptionPane.ERROR_MESSAGE);;
             i--;
             }
             
         } 
-       
-       JOptionPane.showMessageDialog(null, "La suma de los Sueldos es "+ positivo + "\n Los que superan los 1000 son \n"+ suma,  "Media Sueldos   ", JOptionPane.ERROR_MESSAGE);           
+
+        for(int i =0; i <=4; i++ ) { 
+            alturas = Float.parseFloat(JOptionPane.showInputDialog(null, "Ingrese Altura de Alumno "+(i+1), "Recoleccion de Altura ", JOptionPane.ERROR_MESSAGE));
+                if (alturas > 0)
+                {
+                proalturas += alturas;
+
+                if (alturas > 1.75)
+                    {
+                    sumaalt++;  
+                    }
+                }
+                else
+                {
+                    JOptionPane.showMessageDialog(null, "Altura Invalida\n Ingrese un valor Mayor a 0\n",  "Recoleccion de Alturas", JOptionPane.ERROR_MESSAGE);;
+                i--;
+                }    
+                System.out.println(i);
+        if (i >= 4){
+        proalturas /=5; proedades/=5;     
+        JOptionPane.showMessageDialog(null, "Altura Promedio\n  "+proalturas+"\n Mayores de 1,75\n "+sumaalt+"\n  Edad Promedio\n "+proedades+"\n Mayores de 18 Anos\n "+suma,  "Resultados Edades / Alturas", JOptionPane.ERROR_MESSAGE);;
     }
+        
+    
+    }
+
+}
 
 }
