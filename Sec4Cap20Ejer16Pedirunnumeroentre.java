bbbb90e598dae0f4ef436cb1ae48a4fbc1c904e8
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-06-16 21:00:12 -0400

    Sec4Cap20Ejer16Pedirunnumeroentre

--- Sec4Cap20Ejer16Pedirunnumeroentre.java
+++ Sec4Cap20Ejer16Pedirunnumeroentre.java
@@ -2,18 +2,23 @@
 import javax.swing.JOptionPane;
 public class  Sec4Cap20Ejer16Pedirunnumeroentre {
     public static void main (String[]args){
-    int x = 0, numero= Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese Un Numero", "Tabla de Multiplicar", JOptionPane.ERROR_MESSAGE));
-   if (numero > 0 && numero <= 10) {
-    for (int i = 0; x > 10; i++); {
-        System.out.println("entro ciclo for");
-        System.out.println(numero*(++x));
+    int i = 0, numero, multiplicacion;
+    boolean  aplica = false;
+     do {
+        numero= Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese Un Numero", "Tabla de Multiplicar", JOptionPane.ERROR_MESSAGE));
+       if ((numero > 0) && (numero <= 10)){
+        aplica = true;
+       }
+       else {
+        aplica = false;
+        System.out.println("Numero Equivocado");
+       }
         }
-
-       
-    }
-    else 
-    { 
-        JOptionPane.showInputDialog(null, "Ingrese Un Numero Valido", "Tabla de Multiplicar", JOptionPane.ERROR_MESSAGE);
-    }
+        while (aplica == false);{
+            while (i < 10){
+            multiplicacion = numero*(++i);
+            System.out.println(i+"           "+multiplicacion);
+            }
+        }
     }
 }
