7f7f68c2ea3e9fb9c8e44b22f3630c46efc9b9d1
Jorge Arrieta <87jorgearrieta@gmail.com>
2020-07-08 10:27:36 -0400

    se agrega archivo readme

---
+++ curso java 8/Sec4cap21Ejer17Resumenventas.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class  Sec4cap21Ejer17Resumenventas {
+    public static void main  (String[] args) {
+        int codart;
+         int cantlitrs, prelitrs, factotal, cod1art, i = 0, numero, multiplicacion;
+        boolean codartvalido = false;
+        boolean cantlitrsvalido = false;
+        boolean prelitrsvalido = false;
+        Scanner entradacod = new Scanner(System.in);
+        Scanner entradacan = new Scanner(System.in);
+        Scanner entradapre = new Scanner(System.in);
+    while (i > 5);{
+                     do {
+                             System.out.println("Digite Codigo del Articulo");
+                             codart = entradacod.nextInt();
+                             if ((codart > 0 )  && (codart <= 5))
+                            {
+                                    codartvalido = true;
+                            }    
+                            else 
+                            {
+                                    System.out.println("====--- Error ----===");
+                                    System.out.println("Digite Codigo del Articulo CORRECTO");  
+                            }
+                        }
+                    while (codartvalido == false); {
+                             System.out.println("Codigo Guardado");     
+                            }
+                    
+
+                        }
+   
+//###############################################################################   
+    }    
+    
+}
+    
---
+++ curso java 8/Sec4cap21Ejer17Resumenventasfactura/src/main/java/cursojava8/sec4cap21ejer17resumenventasfactura/ejercisio17.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursojava8.sec4cap21ejer17resumenventasfactura;
+
+/**
+ *
+ * @author exjaros
+ */
+public class ejercisio17 {
+    
+}
